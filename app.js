047129012e80b80aa5cc051e0dca89e28216e0b3
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-03 20:44:58 +0100

    setup server

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const port_number = Number.parseInt(process.env.PORT_NUMBER);
+
+server.listen(port_number, () => {
+    console.log(`server listening on port ${port_number}`);
+});
