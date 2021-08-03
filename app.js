4ad592158f708155c5a105edac54c4d5699d497e
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-03 22:55:59 +0100

    add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ const app = express();
 const server = require('http').createServer(app);
 const port_number = Number.parseInt(process.env.PORT_NUMBER);
 
+//use and set express json limit
+app.use(express.json({ limit: '20kb' }));
+
+app.get('/api/products/list', async function (req, res) {
+    try {
+        let body = req.body,
+        user_id = body.user_id,
+        tab = body.tab;
+
+        
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get('/api/products/product_details', async function (req, res) {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+app.get('/api/user/cart', async function (req, res) {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 server.listen(port_number, () => {
     console.log(`server listening on port ${port_number}`);
 });
