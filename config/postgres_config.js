56e66d93efa910b68531b0697b73bc119cf66a51
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-04 17:36:58 +0100

    minor bug fix

--- config/postgres_config.js
+++ config/postgres_config.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://tpkhertoauwmof:ffd09fa57cad8eb975322702b9731bd3f6822551c5dea7531e55e2d181e49fb3@ec2-35-174-56-18.compute-1.amazonaws.com:5432/der5robd57ms1k',
+  connectionString: process.env.DATABASE_URL || process.env.CONN_STRING,
   ssl: {
     rejectUnauthorized: false
   }
