4ecdebb08eb315ded3010cd59c6e59f4d6a85e42
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-04 13:04:36 +0100

    set up boiler plate for routes

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ const port_number = Number.parseInt(process.env.PORT_NUMBER);
 //use and set express json limit
 app.use(express.json({ limit: '20kb' }));
 
+
+app.get('/', async function (req, res) {
+    try {
+        res.status(200).send('Welcome to Nexapp!')
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 app.get('/api/products/list', async function (req, res) {
     try {
         let body = req.body,
-        user_id = body.user_id,
-        tab = body.tab;
+            user_id = body.user_id,
+            tab = body.tab;
 
-        
+
 
     } catch (error) {
         console.error(error);
