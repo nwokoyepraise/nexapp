028dfd64d933530426f0e5a451fbfa9185d21eff
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-09 09:24:31 +0100

    return 404 if product is not found

--- routes/product_detail.js
+++ routes/product_detail.js
@@ -12,8 +12,8 @@ module.exports = function (app) {
             let res0 = await pool.query('SELECT product_id, product_list.seller_id, product_name, unit_price, currency, photo, seller_name, verified, product_desc, delivery_methods, avail_colors, avail_sizes, avail_quantity,' +
                 'pckg_fee, delivery_fee FROM product_list INNER JOIN seller_profile USING (seller_id) WHERE product_id = $1', [product_id]);
 
-            //return empty if returned rowCount is not greater than zero 
-            if (!res0.rowCount > 0) { return res.status(200).send({ status: true, data: [] }); }
+            //return if product is not found i.e !exist
+            if (!res0.rowCount > 0) { return res.status(404).send({ status: false, message: 'Product not found!' }); }
 
             //send data response to user
             res.status(200).send({ status: true, data: res0.rows[0] });
