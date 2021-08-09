a7199ab7b28a2dece0c3ca0aba24492f9f694a3f
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-09 09:16:12 +0100

    remove code redundancy

--- util/null_undefined_checker.js
+++ util/null_undefined_checker.js
@@ -1,4 +1,4 @@
-module.exports = function check_for_null(obj) {
+module.exports = function (obj) {
     let obj_entries = Object.entries(obj);
     for (let i = 0; i < obj_entries.length; i++) {
         if (!obj_entries[i] || !obj_entries[i][1] || obj_entries[i].length < 2) {return {valid: false, field: obj_entries[i][0]} }
