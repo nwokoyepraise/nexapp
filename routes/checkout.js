6f9e69770f2fdfc98d0f82487a27e67203aa73c3
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-13 12:40:41 +0100

    commenting

--- routes/checkout.js
+++ routes/checkout.js
@@ -17,8 +17,6 @@ module.exports.checkout_item = function (app) {
                 cvv = body.cvv,
                 expiry_month = body.expiry_month,
                 expiry_year = body.expiry_year,
-                currency = body.currency,
-                amount = body.amount,
                 fullname = body.fullname,
                 email = body.email,
                 phone_number = body.phone_number;
@@ -26,23 +24,29 @@ module.exports.checkout_item = function (app) {
             //return if user does not exist
             if (!await user_exists(user_id)) { return res.status(406).send({ status: false, message: 'User does not exist!' }); }
 
-            //retrieve item from cart
+            //inner join query for product and shooping cart data on shooping cart and product list tables
             let res0 = await pool.query('SELECT product_id, quantity, user_id, delivery_method, product_color, product_size, product_list.unit_price,' +
                 'product_list.delivery_fee, product_list.pckg_fee, product_list.currency, product_list.avail_quantity, product_list.avail_colors, product_list.avail_sizes,' +
                 'product_list.delivery_methods FROM shopping_cart INNER JOIN product_list USING (product_id) WHERE item_id = $1', [item_id]);
 
 
+            //return if item is not found
             if (!res0.rowCount > 0) { return res.status(404).send({ status: false, message: 'Item not found!' }); }
             res0 = res0.rows[0];
 
+            //check and return if product remaining stock quantity is less than requested quantity
             if (res0.avail_quantity < res0.quantity) { return res.status(404).send({ status: false, message: 'Quantity of item in cart is greater than available stock!' }); }
 
+            //check and return if requested product sizes is no longer available
             if (!res0.avail_sizes.includes(res0.product_size)) { return res.status(404).send({ status: false, message: 'Requested product size is no longer availabe for item!' }); }
 
+            //check and return if requested product color is no longer available
             if (!res0.avail_colors.includes(res0.product_color)) { return res.status(404).send({ status: false, message: 'Requested product color is no longer availabe for item!' }); }
 
+            //check and return if requested delivery method is no longer available for product
             if (!res0.delivery_methods.includes(res0.delivery_method)) { return res.status(404).send({ status: false, message: 'Requested delivery method is no longer availabe for item!' }); }
 
+            //calculate item total price
             let total_price = (res0.unit_price * res0.quantity) + res0.delivery_fee + res0.pckg_fee;
 
             const payload = {
@@ -60,6 +64,7 @@ module.exports.checkout_item = function (app) {
                 "tx_ref": crypt_gen.gen(24)
             }
 
+            //make charge on card
             const response = await flutterwave.Charge.card(payload)
 
             if (response.meta.authorization.mode === 'pin') {
@@ -73,11 +78,13 @@ module.exports.checkout_item = function (app) {
                 }
                 const reCallCharge = await flutterwave.Charge.card(payload2)
 
+                //validate payment
                 const callValidate = await flutterwave.Charge.validate({
                     "otp": "12345",
                     "flw_ref": reCallCharge.data.flw_ref
                 })
 
+                //update data in DB and return transaction data to user
                 if (callValidate.status === 'success') {
                     delete callValidate.data.card;
                     delete callValidate.data.customer;
