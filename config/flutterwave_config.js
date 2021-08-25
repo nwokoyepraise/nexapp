b60569398d602216f67c136fcea298aaf645b86e
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-25 15:55:39 +0100

    remove unwanted files

--- config/flutterwave_config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const flutterwave = require('flutterwave-node-v3');
-const PUBLIC_KEY = process.env.FLUTTER_PUB_KEY;
-const SECRET_KEY = process.env.FLUTTER_SECRET_KEY;
-
-
-
-module.exports =  new flutterwave(PUBLIC_KEY, SECRET_KEY);
--- util/null_undefined_checker.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (obj) {
-    let obj_entries = Object.entries(obj);
-    for (let i = 0; i < obj_entries.length; i++) {
-        if (!obj_entries[i] || !obj_entries[i][1] || obj_entries[i].length < 2) {return {valid: false, field: obj_entries[i][0]} }
-    }
-    return {valid: true};
-}
