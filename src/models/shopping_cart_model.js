49127d2147f6b952e05f0a003672072009a61057
nwokoyepraise <nwokoyepraise@gmail.com>
2021-09-03 05:48:49 +0100

    retrieve currency field

--- src/models/shopping_cart_model.js
+++ src/models/shopping_cart_model.js
@@ -29,7 +29,7 @@ module.exports.get_cart = async function (user_id) {
     try {
         //inner join query for shopping cart product list and seller data on shopping cart, product list and seller profile tables
         return await pool.query('SELECT item_id, product_id, user_id, quantity, delivery_method, product_color, product_size, unit_price, delivery_addr, ' +
-            'product_name, product_desc, photo, product_list.delivery_fee, product_list.pckg_fee, product_list.seller_id, seller_name, verified FROM shopping_cart INNER JOIN product_list USING (product_id) INNER JOIN seller_profile USING (seller_id) WHERE user_id = $1', [user_id]);
+            'product_name, product_desc, photo, product_list.delivery_fee, product_list.pckg_fee, product_list.currency, product_list.seller_id, seller_name, verified FROM shopping_cart INNER JOIN product_list USING (product_id) INNER JOIN seller_profile USING (seller_id) WHERE user_id = $1', [user_id]);
     } catch (error) {
         console.error(error)
     }
