df4983cb705dff891780d83fe0622b72d86a072d
nwokoyepraise <nwokoyepraise@gmail.com>
2021-12-20 15:30:07 +0100

    change server response structure

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 module.exports = router.get('', async function (req, res) {
     try {
-        res.status(200).send('Welcome to Nexapp!');
+        res.status(200).json({ status: true, data: { message: 'server active!' }});
     } catch (error) {
         console.error(error);
     }
